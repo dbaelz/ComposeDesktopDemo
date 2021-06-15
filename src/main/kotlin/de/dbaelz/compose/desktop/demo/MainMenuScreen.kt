543cbe9ebe970faae7a7d68e55689941fc007690
Daniel Bälz <me@dbaelz.de>
2021-06-15 17:36:15 +0200

    Try out background image in Main Menu Screen

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/MainMenuScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/MainMenuScreen.kt
@@ -1,7 +1,6 @@
 package de.dbaelz.compose.desktop.demo
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -9,18 +8,32 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.imageFromResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MainMenuScreen(model: MainMenuModel = MainMenuModel()) {
+    val scrollState = rememberScrollState(0)
+
     Box(
         Modifier
             .padding(4.dp)
             .border(BorderStroke(4.dp, MaterialTheme.colors.primary))
             .fillMaxSize()
     ) {
+        Image(
+            bitmap = imageFromResource("compose-logo.png"),
+            contentDescription = "Compose for Desktop logo",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .alpha(0.4f),
+        )
+
         Column(
             modifier = Modifier.align(Alignment.Center)
+                .fillMaxWidth()
+                .verticalScroll(scrollState),
         ) {
             model.entries.forEach {
                 Button(
@@ -34,6 +47,8 @@ fun MainMenuScreen(model: MainMenuModel = MainMenuModel()) {
 
                 Spacer(Modifier.height(8.dp))
             }
+
+            Spacer(Modifier.height(32.dp))
         }
 
     }
