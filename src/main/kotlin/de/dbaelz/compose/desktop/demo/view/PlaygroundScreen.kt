4692631650564a4f6ae09cdb64972635f45c8b45
Daniel Bälz <me@dbaelz.de>
2021-06-27 16:44:08 +0200

    Some text animation stuff with transition

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/PlaygroundScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/PlaygroundScreen.kt
@@ -1,10 +1,15 @@
 package de.dbaelz.compose.desktop.demo.view
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PlaygroundScreen(onBackNavigation: () -> Unit) {
@@ -14,5 +19,26 @@ fun PlaygroundScreen(onBackNavigation: () -> Unit) {
     ) {
 
         BackButton(onBackNavigation)
+
+        TextInOut("Hello there!")
     }
+}
+
+@Composable
+fun TextInOut(text: String) {
+    val transition = rememberInfiniteTransition()
+
+    val dropText by transition.animateValue(
+        0, text.length, Int.VectorConverter,
+        InfiniteRepeatableSpec(
+            animation = tween(1500),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Text(
+        text = text.dropLast(dropText),
+        fontSize = 32.sp,
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.fillMaxWidth(0.5f)
+    )
 }
