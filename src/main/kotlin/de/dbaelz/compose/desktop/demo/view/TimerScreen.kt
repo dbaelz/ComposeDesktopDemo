e8a4a9daf6fd20af9c3c753a5f23bd36342857fa
Daniel Bälz <me@dbaelz.de>
2021-07-01 17:54:13 +0200

    Update layout of time

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/TimerScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/TimerScreen.kt
@@ -1,16 +1,22 @@
 package de.dbaelz.compose.desktop.demo.view
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import kotlin.math.max
@@ -20,18 +26,17 @@ fun TimerScreen(onBackNavigation: () -> Unit) {
     Column(modifier = Modifier.fillMaxWidth()) {
         BackButton(onBackNavigation)
 
-        Timer(Modifier.size(200.dp).align(Alignment.CenterHorizontally), 10_000)
+        Timer(Modifier.size(280.dp).align(Alignment.CenterHorizontally), 10_000)
     }
 }
 
 @Composable
 fun Timer(
     modifier: Modifier,
-    initialTimerPeriod: Int
+    initialTimerPeriod: Int,
+    colorActive: Color = MaterialTheme.colors.secondary,
+    colorInactive: Color = MaterialTheme.colors.onSecondary
 ) {
-    val colorActive = MaterialTheme.colors.secondary
-    val colorInactive = MaterialTheme.colors.onSecondary
-
     var timerActive by remember { mutableStateOf(false) }
     var periodLeft by remember { mutableStateOf(initialTimerPeriod) }
 
@@ -55,7 +60,7 @@ fun Timer(
                 sweepAngle = FULL_TIMER_ANGLE,
                 size = Size(size.width, size.height),
                 useCenter = false,
-                style = Stroke(width = 10f, cap = StrokeCap.Round)
+                style = Stroke(width = 16f, cap = StrokeCap.Round)
             )
 
             drawArc(
@@ -68,21 +73,35 @@ fun Timer(
             )
         }
 
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.width(100.dp)
+        ) {
             Text(
-                text = if (periodLeft > 0) periodLeft.toString() else "",
-                color = MaterialTheme.colors.primary
+                text = periodLeft.toString(),
+                color = MaterialTheme.colors.secondary,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(MaterialTheme.colors.onSecondary)
             )
             Spacer(modifier = Modifier.height(8.dp))
 
-            Button(onClick = {
-                if (periodLeft <= 0) {
-                    periodLeft = initialTimerPeriod
-                    timerActive = true
-                } else {
-                    timerActive = !timerActive
-                }
-            }) {
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.onSecondary,
+                    contentColor = MaterialTheme.colors.secondary
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    if (periodLeft <= 0) {
+                        periodLeft = initialTimerPeriod
+                        timerActive = true
+                    } else {
+                        timerActive = !timerActive
+                    }
+                }) {
                 Text(
                     text = when {
                         periodLeft <= 0 -> "Restart"
