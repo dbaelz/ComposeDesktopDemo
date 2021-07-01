9fea8a3f6bb6b9f0d07657336d9a9aed0c9497b6
Daniel Bälz <me@dbaelz.de>
2021-07-01 17:26:55 +0200

    Add light color scheme

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/theme/DesktopDemoTheme.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/theme/DesktopDemoTheme.kt
@@ -5,15 +5,38 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 
+
+private val LightPalette = lightColors(
+    primary = Color(0xff4fc3f7),
+    primaryVariant = Color(0xff0093c4),
+    onPrimary = Color(0xff000000),
+    secondary = Color(0xfffdd835),
+    secondaryVariant = Color(0xffc6a700),
+    onSecondary = Color(0xff000000),
+    onSurface = Color(0xff000000),
+    onBackground = Color(0xff000000),
+    error = Color(0xffff3d00),
+    onError = Color(0xff000000)
+)
+
+
 @Composable
-fun DesktopDemoTheme(content: @Composable () -> Unit) {
-    MaterialTheme {
+fun DesktopDemoTheme(
+    isDarkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        // TODO: Add dark theme
+        colors = if (isDarkTheme) LightPalette else LightPalette
+    ) {
         Row(
             Modifier
                 .border(
