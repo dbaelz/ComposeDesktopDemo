6fc662fcc3a24fa2be1e983eb7d96da5350eee34
Daniel Bälz <me@dbaelz.de>
2021-07-06 22:08:42 +0200

    State hoisting for Canvas example

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/CanvasScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/CanvasScreen.kt
@@ -3,28 +3,21 @@ package de.dbaelz.compose.desktop.demo.view
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CanvasScreen(onBackNavigation: () -> Unit) {
-    MenuColumn(
-        onBackNavigation,
-        listOf { PulsingCircle(Modifier.size(600.dp)) },
-    )
-}
-
-@Composable
-fun PulsingCircle(modifier: Modifier = Modifier) {
     val transition = rememberInfiniteTransition()
     val color by transition.animateColor(
         colors.secondary,
@@ -42,13 +35,34 @@ fun PulsingCircle(modifier: Modifier = Modifier) {
             repeatMode = RepeatMode.Reverse
         )
     )
-    val barIndicator by transition.animateValue(
+    val barProgress by transition.animateValue(
         0, BAR_ITEMS + 1, Int.VectorConverter,
         InfiniteRepeatableSpec(
             animation = tween(ANIMATION_DURATION_MILLIS),
             repeatMode = RepeatMode.Reverse
         )
     )
+
+    MenuColumn(
+        onBackNavigation,
+        listOf {
+            PulsingCircle(
+                modifier = Modifier.size(600.dp),
+                color = color,
+                radius = radius,
+                barProgress = barProgress
+            )
+        },
+    )
+}
+
+@Composable
+fun PulsingCircle(
+    modifier: Modifier = Modifier,
+    color: Color,
+    radius: Float,
+    barProgress: Int
+) {
     val colorBarIndicator = colors.primary
     val colorBarIndicatorBackground = colors.secondary
 
@@ -62,7 +76,7 @@ fun PulsingCircle(modifier: Modifier = Modifier) {
 
         (1..BAR_ITEMS).forEach { index ->
             drawRoundRect(
-                color = if (index <= barIndicator) colorBarIndicator else colorBarIndicatorBackground,
+                color = if (index <= barProgress) colorBarIndicator else colorBarIndicatorBackground,
                 topLeft = Offset((BAR_WIDTH * 3) + index * BAR_WIDTH * 1.5f, 20f),
                 size = Size(BAR_WIDTH, 50f),
                 cornerRadius = CornerRadius(5f)
