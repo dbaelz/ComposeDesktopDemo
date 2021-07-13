6223f25b5011d5f936b3db7823fcf6f7cfb4625d
Daniel Bälz <me@dbaelz.de>
2021-07-13 23:56:37 +0200

    Scaffold: Improve drawer and content design

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/ScaffoldScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/ScaffoldScreen.kt
@@ -6,9 +6,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
@@ -27,6 +31,7 @@ fun ScaffoldScreen(onBackNavigation: () -> Unit) {
             }
         },
         drawerContent = { Drawer() },
+        drawerBackgroundColor = MaterialTheme.colors.background.copy(alpha = 0.8f),
         content = { Content() }
     )
 }
@@ -47,12 +52,46 @@ private fun TopBar(onMenuIconClicked: () -> Unit) {
 
 @Composable
 private fun Drawer() {
-    Column {
-        Text("Drawer")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        (0..10).forEach {
+            DrawerEntry(Icons.Default.Star, "Drawer Entry $it")
+        }
+    }
+}
+
+@Composable
+private fun DrawerEntry(icon: ImageVector, text: String) {
+    Row(
+        modifier = Modifier.padding(4.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = MaterialTheme.colors.primary
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.body1,
+        )
     }
 }
 
 @Composable
 private fun Content() {
-    Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.primaryVariant))
+    Box(
+        modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.primaryVariant),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Scaffold Example Content",
+            style = MaterialTheme.typography.h1,
+            textAlign = TextAlign.Center
+        )
+    }
 }
