57c9bc15b3ce1919f827c3ff2ce758f79ea0658f
Daniel Bälz <me@dbaelz.de>
2021-07-20 15:36:04 +0200

    PlaygroundScreen: Example for boxes that always take the max width of the widest sibling text

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/PlaygroundScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/PlaygroundScreen.kt
@@ -1,19 +1,50 @@
 package de.dbaelz.compose.desktop.demo.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PlaygroundScreen(onBackNavigation: () -> Unit) {
     MenuColumn(
         onBackNavigation, listOf(
             {
-                Text(
-                    "Dummy Text",
-                    color = MaterialTheme.colors.onBackground
-                )
+                RowWithChildsMaxWidthText()
             },
         )
     )
+}
+
+@Composable
+private fun RowWithChildsMaxWidthText() {
+    Box(
+        modifier = Modifier.fillMaxSize().background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            Modifier.width(IntrinsicSize.Max).fillMaxHeight()
+        ) {
+            Box(
+                modifier = Modifier.fillMaxHeight().weight(1f)
+                    .background(MaterialTheme.colors.primary)
+            ) {
+                Text(text = "Short text")
+            }
+
+            Spacer(Modifier.width(16.dp))
+
+            Box(
+                modifier = Modifier.fillMaxHeight().weight(1f)
+                    .background(MaterialTheme.colors.secondary)
+            ) {
+                Text(text = "Very very very long text")
+            }
+        }
+    }
 }
