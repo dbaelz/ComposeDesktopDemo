cfffae21535ca4a84006fab67b633e26a74cea20
Daniel Bälz <me@dbaelz.de>
2021-07-22 11:27:24 +0200

    PuzzlerScreen: Add click listener puzzler

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/PuzzlerScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/PuzzlerScreen.kt
@@ -2,12 +2,15 @@ package de.dbaelz.compose.desktop.demo.view
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -18,8 +21,13 @@ fun PuzzlerScreen(onBackNavigation: () -> Unit) {
         onBackNavigation, listOf(
             {
                 Text("ModifierBorderPuzzler", style = MaterialTheme.typography.h3)
-                Spacer(Modifier.width(8.dp))
+                Spacer(Modifier.height(8.dp))
                 ModifierBorderPuzzler()
+            },
+            {
+                Text("ModifierClickPuzzler", style = MaterialTheme.typography.h3)
+                Spacer(Modifier.height(8.dp))
+                ModifierClickPuzzler()
             },
         )
     )
@@ -46,5 +54,28 @@ private fun ModifierBorderPuzzler(borderWidth: Dp = 12.dp) {
             .border(borderWidth, Color.Black)
 
             .padding(borderWidth)
+    )
+}
+
+@ExperimentalFoundationApi
+@Composable
+private fun ModifierClickPuzzler() {
+    ClickableText(Modifier
+        .clickable { println("ModifierClickPuzzler: clickable") }
+        .combinedClickable(onClick = { println("ModifierClickPuzzler: combinedClickable onClick") })
+    )
+}
+
+@ExperimentalFoundationApi
+@Composable
+private fun ClickableText(modifier: Modifier = Modifier) {
+    Text(text = "Click me!",
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .width(200.dp)
+            .height(32.dp)
+            .border(4.dp, Color.LightGray)
+            .clickable { println("ClickableText: clickable") }
+            .combinedClickable(onClick = { println("ClickableText: combinedClickable onClick") })
     )
 }
