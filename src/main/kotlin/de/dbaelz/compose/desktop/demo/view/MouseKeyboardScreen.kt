6adc7a8b6cd130d17a69562bbb4ebc2c2d208abd
Daniel Bälz <me@dbaelz.de>
2021-07-25 23:04:53 +0200

    MouseKeyboardScreen: Simple mouse click example for single, double and long click

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/MouseKeyboardScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/MouseKeyboardScreen.kt
@@ -1,16 +1,60 @@
 package de.dbaelz.compose.desktop.demo.view
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
+@ExperimentalFoundationApi
 @Composable
 fun MouseKeyboardScreen(onBackNavigation: () -> Unit) {
+    var lastClickLabel by remember { mutableStateOf("") }
+
     MenuColumn(
-        onBackNavigation, listOf(
-            {
-                Text("Mouse & Keyboard", style = MaterialTheme.typography.h1)
-            },
-        )
+        onBackNavigation, listOf {
+            MouseClick(modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = { lastClickLabel = "Click" },
+                onLongClick = { lastClickLabel = "LongClick" },
+                onDoubleClick = { lastClickLabel = "DoubleClick" })
+
+            Spacer(Modifier.height(8.dp))
+
+            Text(text = lastClickLabel, style = MaterialTheme.typography.h3)
+        }
     )
+}
+
+@ExperimentalFoundationApi
+@Composable
+private fun MouseClick(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    onLongClick: () -> Unit = {},
+    onDoubleClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .size(200.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colors.primary)
+            .combinedClickable(
+                onClick = onClick,
+                onLongClick = onLongClick,
+                onDoubleClick = onDoubleClick
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Click me")
+    }
 }
