b838b21c0d280923aa383dd17252e40d8a2d97d3
Daniel Bälz <me@dbaelz.de>
2021-08-03 21:19:09 +0200

    KeyboardScreen: Add simple text field example

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/KeyboardScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/KeyboardScreen.kt
@@ -1,13 +1,35 @@
 package de.dbaelz.compose.desktop.demo.view
 
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun KeyboardScreen(onBackNavigation: () -> Unit) {
     Screen(
         { ScreenTopBar("Keyboard Input and Focus", onBackNavigation) },
-        listOf {
+        listOf(
+            {
+                val state = remember { mutableStateOf(TextFieldValue()) }
 
-        }
+                TextField(
+                    modifier = Modifier.width(400.dp),
+                    value = state.value,
+                    onValueChange = { state.value = it },
+                    label = { Text("Your Bio") },
+                    placeholder = { Text("Tell us something about yourself") }
+                )
+            },
+            {
+
+            }
+        )
     )
 }
+
