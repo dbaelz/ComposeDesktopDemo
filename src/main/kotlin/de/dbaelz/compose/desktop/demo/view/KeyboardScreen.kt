4471666c234902ac85d3917c3da3d2caaa246a56
Daniel Bälz <me@dbaelz.de>
2021-08-04 22:16:19 +0200

    KeyboardScreen: Add example key handling for TextField

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/KeyboardScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/KeyboardScreen.kt
@@ -3,25 +3,40 @@ package de.dbaelz.compose.desktop.demo.view
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 
+@ExperimentalComposeUiApi
 @Composable
 fun KeyboardScreen(onBackNavigation: () -> Unit) {
     Screen(
         { ScreenTopBar("Keyboard Input and Focus", onBackNavigation) },
         listOf(
             {
-                val state = remember { mutableStateOf(TextFieldValue()) }
+                var state by remember { mutableStateOf(TextFieldValue()) }
+
+                Text(text = "Press \"CTRL + D\" for a sample text")
 
                 TextField(
-                    modifier = Modifier.width(400.dp),
-                    value = state.value,
-                    onValueChange = { state.value = it },
+                    modifier = Modifier
+                        .width(400.dp)
+                        .onPreviewKeyEvent {
+                            if (it.isCtrlPressed && it.key == Key.D) {
+                                state = TextFieldValue("I am ...")
+                                true
+                            } else {
+                                false
+                            }
+                        },
+                    value = state,
+                    onValueChange = { state = it },
                     label = { Text("Your Bio") },
                     placeholder = { Text("Tell us something about yourself") }
                 )
