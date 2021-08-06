0be217efdd9f91c4f1c5f00e321d52d2f6ffb781
Daniel Bälz <me@dbaelz.de>
2021-08-06 20:03:01 +0200

    PlaygroundScreen: Add non-interactive preview with the Compose Multiplatform plugin

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/PlaygroundScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/PlaygroundScreen.kt
@@ -1,5 +1,6 @@
 package de.dbaelz.compose.desktop.demo.view
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,8 +11,14 @@ fun PlaygroundScreen(onBackNavigation: () -> Unit) {
         { ScreenTopBar("Playground", onBackNavigation) },
         listOf(
             {
-                Text("Playground", style = MaterialTheme.typography.h1)
+                Playground()
             },
         )
     )
+}
+
+@Preview
+@Composable
+private fun Playground() {
+    Text("Playground", style = MaterialTheme.typography.h1)
 }
