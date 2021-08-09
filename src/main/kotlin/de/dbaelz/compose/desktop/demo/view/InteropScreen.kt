20b3949d584e7af3a22869d63d3a060839f99159
Daniel Bälz <me@dbaelz.de>
2021-08-09 22:39:08 +0200

    InteropScreen: Add example that uses a Composable inside Swing with ComposePanel

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/InteropScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/InteropScreen.kt
@@ -1,16 +1,15 @@
 package de.dbaelz.compose.desktop.demo.view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.LocalContentColor
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.ComposePanel
 import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.unit.dp
 import javax.swing.JButton
+import javax.swing.JFrame
 
 @Composable
 fun InteropScreen(onBackNavigation: () -> Unit) {
@@ -19,7 +18,10 @@ fun InteropScreen(onBackNavigation: () -> Unit) {
         listOf(
             {
                 SwingJButton()
-            }
+            },
+            {
+                ComposableInsideSwing()
+            },
         )
     )
 }
@@ -52,4 +54,44 @@ private fun SwingJButton() {
         }
     }
 
+}
+
+@Composable
+private fun ComposableInsideSwing() {
+    var counter by remember { mutableStateOf(0) }
+
+    SwingPanel(
+        background = MaterialTheme.colors.primary,
+        modifier = Modifier.width(400.dp).height(200.dp),
+        factory = {
+            val composePanel = ComposePanel()
+
+            composePanel.setContent {
+                Counter(counter) { counter++ }
+            }
+
+            return@SwingPanel JFrame().add(composePanel)
+        }
+    )
+}
+
+@Composable
+private fun Counter(counter: Int, onClick: () -> Unit) {
+    CompositionLocalProvider(
+        LocalTextStyle provides MaterialTheme.typography.h5,
+        LocalContentColor provides MaterialTheme.colors.onBackground
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = onClick) {
+                Text("Increase Counter")
+            }
+
+            Spacer(Modifier.height(8.dp))
+
+            Text("Counter: $counter")
+        }
+    }
 }
