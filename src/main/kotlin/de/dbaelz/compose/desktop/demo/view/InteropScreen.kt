b967dd4f5cd83d00e01d7488d090f45f67059b74
Daniel Bälz <me@dbaelz.de>
2021-09-08 12:12:50 +0200

    InteropScreen: Swing window with JFrame inside Composable

--- src/main/kotlin/de/dbaelz/compose/desktop/demo/view/InteropScreen.kt
+++ src/main/kotlin/de/dbaelz/compose/desktop/demo/view/InteropScreen.kt
@@ -8,20 +8,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposePanel
 import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.unit.dp
-import javax.swing.JButton
-import javax.swing.JFrame
+import java.awt.Dimension
+import javax.swing.*
 
 @Composable
 fun InteropScreen(onBackNavigation: () -> Unit) {
     Screen(
         { ScreenTopBar("Interoperability with Swing", onBackNavigation) },
         listOf(
-            {
-                SwingJButton()
-            },
-            {
-                ComposableInsideSwing()
-            },
+            { SwingJButton() },
+            { SwingWindowButton() },
+            { ComposableInsideSwing() },
         )
     )
 }
@@ -38,7 +35,7 @@ private fun SwingJButton() {
             background = MaterialTheme.colors.background,
             modifier = Modifier.width(400.dp).height(50.dp),
             factory = {
-                return@SwingPanel JButton("Increase Counter").apply {
+                return@SwingPanel JButton("JButton inside Composable").apply {
                     addActionListener { counter++ }
                 }
             }
@@ -56,12 +53,49 @@ private fun SwingJButton() {
 
 }
 
+
+@Composable
+private fun SwingWindowButton() {
+    var windowOpened by remember { mutableStateOf(false) }
+
+    Button(onClick = {
+        windowOpened = true
+    }) {
+        Text("Open Swing Window")
+
+        if (windowOpened) {
+            windowOpened = false
+            SwingWindow()
+        }
+    }
+}
+
+@Composable
+private fun SwingWindow() {
+    SwingPanel(
+        background = MaterialTheme.colors.background,
+        modifier = Modifier.width(400.dp).height(50.dp),
+        factory = {
+            JPanel().apply {
+                JFrame("Hello World Swing").apply {
+                    preferredSize = Dimension(320, 200)
+
+                    contentPane.add(JLabel("Hello World", SwingConstants.CENTER))
+
+                    pack()
+                    isVisible = true
+                }
+            }
+        }
+    )
+}
+
 @Composable
 private fun ComposableInsideSwing() {
     var counter by remember { mutableStateOf(0) }
 
     SwingPanel(
-        background = MaterialTheme.colors.primary,
+        background = MaterialTheme.colors.surface,
         modifier = Modifier.width(400.dp).height(200.dp),
         factory = {
             val composePanel = ComposePanel()
@@ -70,7 +104,7 @@ private fun ComposableInsideSwing() {
                 Counter(counter) { counter++ }
             }
 
-            return@SwingPanel JFrame().add(composePanel)
+            return@SwingPanel JPanel().add(composePanel)
         }
     )
 }
@@ -86,7 +120,7 @@ private fun Counter(counter: Int, onClick: () -> Unit) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Button(onClick = onClick) {
-                Text("Increase Counter")
+                Text("Composable inside Swing")
             }
 
             Spacer(Modifier.height(8.dp))
